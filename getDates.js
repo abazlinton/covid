e0701c1df55789b403bc5f30afa74e8e36a2aaec
Alex Bazlinton <abazlinton@gmail.com>
2021-01-31 17:18:13 +0000

    Change point of filtering. Convert date to String

--- getDates.js
+++ getDates.js
@@ -2,8 +2,8 @@ XLSX = require('xlsx');
 
 const workbook = XLSX.readFile('./daily.xlsx');
 const rows = XLSX.utils.sheet_to_json(workbook.Sheets['Table 5 - Testing'])
-const dates = rows.map(getDateAndPositivityRate)
-const datesWithValidData = dates.filter(date => !isNaN(date.positivityRate))
+const datesWithValidData = rows.filter(row => !isNaN(row.__EMPTY_10))
+const dates = datesWithValidData.map(getDateAndPositivityRate)
 
 function getDateAndPositivityRate(row) {
     return {
@@ -13,7 +13,7 @@ function getDateAndPositivityRate(row) {
 }
 
 function SerialDateToJSDate(serialDate, offsetUTC) {
-    return new Date(Date.UTC(0, 0, serialDate, offsetUTC));
+    return new Date(Date.UTC(0, 0, serialDate, offsetUTC)).toISOString();
 }
 
-module.exports = datesWithValidData
+module.exports = dates
